e31781b06431a51e34bd0617fae79ca14a737148
TheR20 <r20_banana@hotmail.com>
2021-02-21 18:12:24 -0600

    Juego creado con cuadrados y bug si se pica demaciado rapido el cuadrado sale volando

--- App.js
+++ App.js
@@ -1,114 +1,153 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { Component } from 'react';
+import { StyleSheet, View,TouchableOpacity,Text } from 'react-native';
+import Matter from "matter-js";
+import { GameEngine } from "react-native-game-engine";
+import Bird from './src/components/Bird';
+import Wall from './src/components/Wall';
+import Constants from './src/Constants';
+import Physics from './src/Physics';
+export const randomBetween = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+export const generatePipes = () => {
+    let topPipeHeight = randomBetween(100, (Constants.MAX_HEIGHT / 2) - 100);
+    let bottomPipeHeight = Constants.MAX_HEIGHT - topPipeHeight - Constants.GAP_SIZE;
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+    let sizes = [topPipeHeight, bottomPipeHeight]
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
+    if (Math.random() < 0.5) {
+        sizes = sizes.reverse();
+    }
+
+
+    return sizes;
+}
+
+export default class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            running: true
+        };
+
+        this.gameEngine = null;
+
+        this.entities = this.setupWorld();
+    }
+
+    setupWorld = () => {
+        let engine = Matter.Engine.create({ enableSleeping: false });
+        let world = engine.world;
+        world.gravity.y = 1.2;
+
+        let bird = Matter.Bodies.rectangle( Constants.MAX_WIDTH / 4, Constants.MAX_HEIGHT / 2, 50, 50);
+        bird.restitution = 20;
+        let floor = Matter.Bodies.rectangle( Constants.MAX_WIDTH / 2, Constants.MAX_HEIGHT - 100, Constants.MAX_WIDTH, 50, { isStatic: true });
+        let ceiling = Matter.Bodies.rectangle( Constants.MAX_WIDTH / 2, 25, Constants.MAX_WIDTH, 50, { isStatic: true });
+
+        let [pipe1Height, pipe2Height] = generatePipes();
+
+        let pipe1 = Matter.Bodies.rectangle( Constants.MAX_WIDTH - (Constants.PIPE_WIDTH / 2), pipe1Height / 2, Constants.PIPE_WIDTH, pipe1Height, { isStatic: true });
+        let pipe2 = Matter.Bodies.rectangle( Constants.MAX_WIDTH - (Constants.PIPE_WIDTH / 2), Constants.MAX_HEIGHT - (pipe2Height / 2), Constants.PIPE_WIDTH, pipe2Height, { isStatic: true });
+
+        let [pipe3Height, pipe4Height] = generatePipes();
+
+        let pipe3 = Matter.Bodies.rectangle( Constants.MAX_WIDTH * 2 - (Constants.PIPE_WIDTH / 2), pipe3Height / 2, Constants.PIPE_WIDTH, pipe3Height, { isStatic: true });
+        let pipe4 = Matter.Bodies.rectangle( Constants.MAX_WIDTH * 2 - (Constants.PIPE_WIDTH / 2), Constants.MAX_HEIGHT - (pipe4Height / 2), Constants.PIPE_WIDTH, pipe4Height, { isStatic: true });
+
+        Matter.World.add(world, [bird, floor, ceiling, pipe1, pipe2, pipe3, pipe4]);
+        Matter.Events.on(engine, 'collisionStart', (event) => {
+            var pairs = event.pairs;
+
+            this.gameEngine.dispatch({ type: "game-over"});
+
+        });
+
+        return {
+            physics: { engine: engine, world: world },
+            floor: { body: floor, size: [Constants.MAX_WIDTH, 50], color: "green", renderer: Wall },
+            ceiling: { body: ceiling, size: [Constants.MAX_WIDTH, 50], color: "green", renderer: Wall },
+            bird: { body: bird, size: [50, 50], color: 'red', renderer: Bird},
+            pipe1: { body: pipe1, size: [Constants.PIPE_WIDTH, pipe1Height], color: "green", renderer: Wall },
+            pipe2: { body: pipe2, size: [Constants.PIPE_WIDTH, pipe2Height], color: "green", renderer: Wall },
+            pipe3: { body: pipe3, size: [Constants.PIPE_WIDTH, pipe3Height], color: "green", renderer: Wall },
+            pipe4: { body: pipe4, size: [Constants.PIPE_WIDTH, pipe4Height], color: "green", renderer: Wall }
+        }
+    }
+
+    onEvent = (e) => {
+        if (e.type === "game-over"){
+            //Alert.alert("Game Over");
+            this.setState({
+                running: false
+            });
+        }
+    }
+
+    reset = () => {
+        this.gameEngine.swap(this.setupWorld());
+        this.setState({
+            running: true
+        });
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <GameEngine
+                    ref={(ref) => { this.gameEngine = ref; }}
+                    style={styles.gameContainer}
+                    systems={[Physics]}
+                    running={this.state.running}
+                    onEvent={this.onEvent}
+                    entities={this.entities}>
+
+                </GameEngine>
+                {!this.state.running && <TouchableOpacity style={styles.fullScreenButton} onPress={this.reset}>
+                    <View style={styles.fullScreen}>
+                        <Text style={styles.gameOverText}>Game Over</Text>
+                    </View>
+                </TouchableOpacity>}
             </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+        );
+    }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    gameContainer: {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+    },
+    gameOverText: {
+        color: 'white',
+        fontSize: 48
+    },
+    fullScreen: {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: 'black',
+        opacity: 0.8,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    fullScreenButton: {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        flex: 1
+    }
 });
-
-export default App;
